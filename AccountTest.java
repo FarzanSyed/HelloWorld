9549e42eb5dedea2a08000d7e66f27bebf00514b
Farzan <farzansyed16@gmail.com>
2018-09-18 17:28:37 -0400

    Added three java files

---
+++ AccountTest.java
@@ -0,0 +1,26 @@
+//AccountTest
+//Create and manipulate an Account object
+
+import java.util.Scanner;
+
+public class AccountTest{
+  public static void main(String[] args){
+    //Create object of Scanner
+    Scanner input = new Scanner(System.in);
+    
+    //create an Account object and assign it to myAccount
+    Account myAccount = new Account();
+    
+    //display initial value of name(mull)
+    System.out.printf("initial name is %s%n%n", myAccount.getName());
+    
+    //prompt for and read name
+    System.out.println("Please enter the name:");
+    String theName = input.nextLine(); // read a line of text
+    myAccount.setName(theName); // put theName in myAccount
+    System.out.println(); //outputs a blank line
+    
+    //display the name stored in object myAccount
+    System.out.printf("Name in object myAccount is: %n%s%n", myAccount.getName());
+  }
+}
---
+++ Arithmetic.java
@@ -0,0 +1,5 @@
+public class ArithmeticExpressions{
+  public static void main(String[] args){
+    System.out.println( 20 - 5 / 2 + 3);
+  }
+}
